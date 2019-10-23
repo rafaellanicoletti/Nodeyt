5b23be13cf504374dc1b8140caad0c2ad0839f2c
rafaella nicoletti <rafaella_np@hotmail.com>
2019-10-23 13:44:50 +0200

    app js

--- server.js
+++ server.js
@@ -1,9 +1,27 @@
+// var http = require('http');
+
+// function onRequest(req,res){
+//     res.writeHead(200,{'Content-Type' : 'text/plain'});
+//     res.write('Hello World');
+//     res.end();
+// }
+
+// http.createServer(onRequest).listen(8000);
+
 var http = require('http');
+var fs = require('fs');
 
-function onRequest(req,res){
-    res.writeHead(200,{'Content-Type' : 'text/plain'});
-    res.write('Hello World');
-    res.end();
-}
 
-http.createServer(onRequest).listen(8000);
+http.createServer(function onRequest(req, res) {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    fs.readFile('./index.html', null, function(err,data){
+        if (err){
+            res.writeHead(404);
+            res.write('File not found');
+        } else{
+            res.write(data);
+        }
+        res.end();
+    })
+   
+}).listen(8000);
